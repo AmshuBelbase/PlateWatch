0d78e6a60d4e2b34f61b52a1490bb3b133cea19c
Amshu Belbase <amsubelbs@gmail.com>
2023-09-18 17:33:15 +0530

    Apk Splash Update

--- Java Apk/PlateWatch/app/src/main/java/com/example/platewatch/SplashScreen.java
+++ Java Apk/PlateWatch/app/src/main/java/com/example/platewatch/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.platewatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+
+            }
+        }, 3500);
     }
 }
