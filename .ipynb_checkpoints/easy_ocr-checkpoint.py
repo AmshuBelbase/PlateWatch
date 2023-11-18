7a64c415f1ccba3528f3779e0cb488e5f3a10966
Amshu Belbase <amsubelbs@gmail.com>
2023-11-18 18:17:41 +0530

    Deleted update

--- .ipynb_checkpoints/easy_ocr-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import easyocr
-import matplotlib.pyplot as plt
-import cv2
-from IPython.display import Image, display
-display(Image('plates/scaned_img_0.jpg')) 
-reader = easyocr.Reader(['en'])
-output = reader.readtext('plates/scaned_img_2.jpg')
-output
